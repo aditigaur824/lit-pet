3e635cb50208825d211864792878d97e96c8e952
Ivan Cheung <ivan.j.cheung@gmail.com>
2020-08-11 01:05:36 -0700

    remove os dependancy

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const os = require('os');
 const path = require('path');
 const express = require('express');
 const router = express.Router();
